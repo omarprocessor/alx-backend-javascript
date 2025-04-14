0abaa8da3040e3771fdad1abf12210671c2c835e
Omar Mohumed <wadaniinarais1@gmail.com>
2025-04-14 19:55:54 +0000

    task done

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const key = `$${departmentName}`;
-  const value = employees.map(employee => `$${employee}`);
   return {
-    [key]: value,
+    [departmentName]: [
+      ...employees,
+    ],
   };
 }
