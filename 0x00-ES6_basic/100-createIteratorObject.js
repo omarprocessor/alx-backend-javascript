17c16a5c495315e0a4feaf913657a9f0b93d8e4b
Omar Mohumed <wadaniinarais1@gmail.com>
2025-04-14 19:50:28 +0000

    task done

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,17 +1,9 @@
 export default function createIteratorObject(report) {
-  const employees = Object.values(report.allEmployees).flat();
-  let index = 0;
-
-  return {
-    [Symbol.iterator]: function() {
-      return {
-        next: function() {
-          if (index < employees.length) {
-            return { value: employees[index++], done: false };
-          }
-          return { done: true };
-        }
-      };
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
     }
-  };
+  }());
 }
