bafa3f4a2089040177a8d581a4d346ca2cbdda01
Omar Mohumed <wadaniinarais1@gmail.com>
2025-04-16 23:07:20 +0000

    task done

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     uploadPhoto(fileName),
-    signUpUser(firstName, lastName)
-  ]);
+    signUpUser(firstName, lastName),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
