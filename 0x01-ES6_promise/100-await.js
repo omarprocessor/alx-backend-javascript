ba83a151f9d66f30c2553ed2ff4a7f68547f9342
Omar Mohumed <wadaniinarais1@gmail.com>
2025-04-16 23:22:57 +0000

    task done

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+import uploadPhoto from './utils';
+import createUser from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch {
+    return { photo: null, user: null };
+  }
+}
