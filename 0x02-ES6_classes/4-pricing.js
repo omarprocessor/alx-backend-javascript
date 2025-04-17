044acbcbbb5fa1145a48859f566f6a95d885b031
Omar Mohumed <wadaniinarais1@gmail.com>
2025-04-17 21:35:29 +0000

    task done

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') throw new TypeError('Amount must be a number');
+    if (!(currency instanceof Currency)) throw new TypeError('Currency must be a Currency');
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') throw new TypeError('Amount must be a number');
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) throw new TypeError('Currency must be a Currency');
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') throw new TypeError('Amount must be a number');
+    if (typeof conversionRate !== 'number') throw new TypeError('Conversion rate must be a number');
+    return amount * conversionRate;
+  }
+}
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new TypeError('sqft must be a number');
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor === Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    // This method is intended to be overridden by subclasses
+    return this;
+  }
+}
