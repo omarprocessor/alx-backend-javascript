9492cdc95f15c50efa3fdfc9e30b1ec57d0ac541
Omar Mohumed <wadaniinarais1@gmail.com>
2025-04-24 01:35:47 +0000

    task done

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,8 @@
 function getStudentIdsSum(list) {
-if (!Array.isArray(list)) {
-return 0
-}
-return list.reduce((sum, student) => sum + student.id, 0)
+  if (!Array.isArray(list)) {
+    return 0;
+  }
+  return list.reduce((sum, student) => sum + student.id, 0);
 }
 
-module.exports = getStudentIdsSum
+module.exports = getStudentIdsSum;
