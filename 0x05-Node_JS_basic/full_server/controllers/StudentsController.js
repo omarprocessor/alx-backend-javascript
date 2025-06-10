2cfa6d33c849f15e1a2797e543bb31e89dfbe434
Omar Mohumed <wadaniinarais1@gmail.com>
2025-06-11 02:07:02 +0300

    task 8

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,14 +4,14 @@ class StudentsController {
   static async getAllStudents(req, res) {
     try {
       const database = await readDatabase(req.app.get('databaseFile'));
-      
+
       let response = 'This is the list of our students\n';
-      
-      Object.keys(database).sort().forEach(field => {
+
+      Object.keys(database).sort().forEach((field) => {
         const students = database[field];
         response += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
       });
-      
+
       res.status(200).send(response);
     } catch (error) {
       res.status(500).send('Cannot load the database');
@@ -20,20 +20,22 @@ class StudentsController {
 
   static async getAllStudentsByMajor(req, res) {
     try {
-      const major = req.params.major;
+      const { major } = req.params;
       if (major !== 'CS' && major !== 'SWE') {
         return res.status(500).send('Major parameter must be CS or SWE');
       }
 
       const database = await readDatabase(req.app.get('databaseFile'));
-      
+
       if (!database[major]) {
         return res.status(500).send('Cannot load the database');
       }
 
       res.status(200).send(`List: ${database[major].join(', ')}`);
+      return null;
     } catch (error) {
       res.status(500).send('Cannot load the database');
+      return null;
     }
   }
 }
