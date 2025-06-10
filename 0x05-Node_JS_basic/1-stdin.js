e3f2805e0c332289bbfb7aef85b528fd75a82d01
Omar Mohumed <wadaniinarais1@gmail.com>
2025-06-11 00:50:03 +0300

    added 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.stdin.pause();
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
