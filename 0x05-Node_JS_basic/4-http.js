3747af4080283db1836b253c25bc82d873afbd54
Omar Mohumed <wadaniinarais1@gmail.com>
2025-06-11 01:14:52 +0300

    added 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,20 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello ALX!';
+
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello ALX!');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-app.listen(1245);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
 module.exports = app;
