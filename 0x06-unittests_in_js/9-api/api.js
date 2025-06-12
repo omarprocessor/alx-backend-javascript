2e1addeb6eee944349f5be702ead2478e55ce0ff
Omar Mohumed <wadaniinarais1@gmail.com>
2025-06-12 22:11:20 +0300

    updated task 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,40 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require('express')
 
-const PORT = 4000;
+const app = express()
+const port = 7865
 
-// Cart route
-app.get('/cart/:id', (req, res) => {
-  const { id } = req.params;
-  if (!/^[0-9]+$/.test(id)) {
-    res.status(404).send('Not Found');
-    return;
-  }
-  res.send(`Payment methods for cart ${id}`);
-});
-
-// Root route
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
-});
+ res.send('Welcome to the payment system')
+})
 
-// 404 handler
-app.use((req, res) => {
-  res.status(404).send('Not Found');
-});
+// Route with numeric :id validation using regex
+app.get('/cart/:id(\\d+)', (req, res) => {
+ const id = req.params.id
+ res.send(`Payment methods for cart ${id}`)
+})
 
-// Error handler
-app.use((err, req, res) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+app.listen(port, () => {
+ console.log(`API available on localhost port ${port}`)
+})
 
-module.exports = app;
-
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`API available on localhost port ${PORT}`);
-  });
-}
-
-module.exports = app;
+module.exports = app
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,26 +1,49 @@
-const request = require('supertest');
-const app = require('./api');
+const request = require('request')
+const { expect } = require('chai')
 
-describe('Cart API', function() {
-  it('should return correct status code for valid cart', function(done) {
-    request(app)
-      .get('/cart/123')
-      .expect(200)
-      .end(done);
-  });
+describe('Index page', () => {
+ it('should return status code 200', (done) => {
+   request.get('http://localhost:7865', (err, res, body) => {
+     expect(res.statusCode).to.equal(200)
+     done()
+   })
+ })
 
-  it('should return correct status code for invalid cart', function(done) {
-    request(app)
-      .get('/cart/abc')
-      .expect(404)
-      .end(done);
-  });
+ it('should return correct message', (done) => {
+   request.get('http://localhost:7865', (err, res, body) => {
+     expect(body).to.equal('Welcome to the payment system')
+     done()
+   })
+ })
 
-  it('should return correct content for valid cart', function(done) {
-    request(app)
-      .get('/cart/123')
-      .expect(200)
-      .expect('Payment methods for cart 123')
-      .end(done);
-  });
-});
+ it('should not return 404', (done) => {
+   request.get('http://localhost:7865', (err, res, body) => {
+     expect(res.statusCode).to.not.equal(404)
+     done()
+   })
+ })
+})
+
+describe('Cart page', () => {
+ it('should return 200 when :id is a number', (done) => {
+   request.get('http://localhost:7865/cart/12', (err, res, body) => {
+     expect(res.statusCode).to.equal(200)
+     expect(body).to.equal('Payment methods for cart 12')
+     done()
+   })
+ })
+
+ it('should return 404 when :id is NOT a number', (done) => {
+   request.get('http://localhost:7865/cart/hello', (err, res, body) => {
+     expect(res.statusCode).to.equal(404)
+     done()
+   })
+ })
+
+ it('should return 404 when :id is missing', (done) => {
+   request.get('http://localhost:7865/cart/', (err, res, body) => {
+     expect(res.statusCode).to.equal(404)
+     done()
+   })
+ })
+})
